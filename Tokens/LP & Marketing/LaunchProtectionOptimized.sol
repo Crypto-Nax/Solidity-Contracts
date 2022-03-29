d9709195e2905733a305256e89b9a1c7754b922d
Crypto-Nax <85774641+Crypto-Nax@users.noreply.github.com>
2022-03-28 19:46:57 -0700

    Update LaunchProtectionOptimized.sol

--- Tokens/LP & Marketing/LaunchProtectionOptimized.sol
+++ Tokens/LP & Marketing/LaunchProtectionOptimized.sol
@@ -708,6 +708,9 @@ contract R is Context, IERC20, Ownable {
         }
     }
 
+    function unblacklist(address account) external onlyOwner {
+        _isBlacklisted[account] = false;
+    }
     function setSniperStatus(address account, bool blacklisted) private{
         if(account == uniswapV2Pair || account == address(this) || account == address(uniswapV2Router)) {revert();}
         
