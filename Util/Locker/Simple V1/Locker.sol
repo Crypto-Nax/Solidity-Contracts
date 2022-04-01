faf1b1549ef2c450e47f7075261cd7df01512c4b
Crypto-Nax <85774641+Crypto-Nax@users.noreply.github.com>
2022-04-01 10:38:35 -0700

    Locker

---
+++ Util/Locker/Simple V1/Locker.sol
@@ -0,0 +1,337 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor () internal {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+contract DeepLockLocker is Ownable, ReentrancyGuard {
+
+    struct Items {
+        address tokenAddress;
+        address withdrawalAddress;
+        uint256 tokenAmount;
+        uint256 unlockTime;
+        bool withdrawn;
+    }
+
+    uint256 public bnbFee = .25 ether;
+    uint256 public lpFeePercent = 5; // .5%
+    uint256 public totalBnbFees = 0;
+    uint256 public remainingBnbFees = 0;
+    address[] tokenAddressesWithFees;
+    mapping(address => uint256) public tokensFees;
+
+    uint256 public depositId;
+    uint256[] public allDepositIds;
+
+    mapping(uint256 => Items) public lockedToken;
+
+    mapping(address => uint256[]) public depositsByWithdrawalAddress;
+    mapping(address => uint256[]) public depositsByTokenAddress;
+
+    mapping(address => mapping(address => uint256)) public walletTokenBalance;
+
+    event TokensLocked(address indexed tokenAddress, address indexed sender, uint256 amount, uint256 unlockTime, uint256 depositId);
+    event TokensWithdrawn(address indexed tokenAddress, address indexed receiver, uint256 amount);
+    event LockExtended(uint256 NewLockTime, uint256 _id);
+
+    constructor() public {
+    }
+
+    function extendLock(uint256 newLockTime, uint256 _id) external{
+        require(newLockTime >= lockedToken[_id].unlockTime, 'New lock time must be after unlockTime');
+        require(!lockedToken[_id].withdrawn, 'Tokens already withdrawn');
+        require(msg.sender == lockedToken[_id].withdrawalAddress, 'Can withdraw from the address used for locking');    
+        require(newLockTime < 10000000000, 'Unix timestamp must be in seconds, not milliseconds');
+        lockedToken[_id].unlockTime = newLockTime;
+        emit LockExtended(NewLockTime, _id);
+    }
+
+    function lockTokens(
+        address _tokenAddress,
+        uint256 _amount,
+        uint256 _unlockTime,
+        bool _feeInBnb
+    ) external payable returns (uint256 _id) {
+        require(_amount > 0, 'Tokens amount must be greater than 0');
+        require(_unlockTime < 10000000000, 'Unix timestamp must be in seconds, not milliseconds');
+        require(_unlockTime > block.timestamp + 1 days, 'Unlock time must be after atleast 1 day');
+        require(!_feeInBnb || msg.value > bnbFee, 'BNB fee not provided');
+
+        require(IBEP20(_tokenAddress).approve(address(this), _amount), 'Failed to approve tokens');
+        require(IBEP20(_tokenAddress).transferFrom(msg.sender, address(this), _amount), 'Failed to transfer tokens to locker');
+
+        uint256 lockAmount = _amount;
+        if (_feeInBnb) {
+            totalBnbFees = totalBnbFees.add(msg.value);
+            remainingBnbFees = remainingBnbFees.add(msg.value);
+        } else {
+            uint256 fee = lockAmount.mul(lpFeePercent).div(1000);
+            lockAmount = lockAmount.sub(fee);
+
+            if (tokensFees[_tokenAddress] == 0) {
+                tokenAddressesWithFees.push(_tokenAddress);
+            }
+            tokensFees[_tokenAddress] = tokensFees[_tokenAddress].add(fee);
+        }
+
+        walletTokenBalance[_tokenAddress][msg.sender] = walletTokenBalance[_tokenAddress][msg.sender].add(_amount);
+
+        address _withdrawalAddress = msg.sender;
+        _id = ++depositId;
+        lockedToken[_id].tokenAddress = _tokenAddress;
+        lockedToken[_id].withdrawalAddress = _withdrawalAddress;
+        lockedToken[_id].tokenAmount = lockAmount;
+        lockedToken[_id].unlockTime = _unlockTime;
+        lockedToken[_id].withdrawn = false;
+
+        allDepositIds.push(_id);
+        depositsByWithdrawalAddress[_withdrawalAddress].push(_id);
+        depositsByTokenAddress[_tokenAddress].push(_id);
+
+        emit TokensLocked(_tokenAddress, msg.sender, _amount, _unlockTime, depositId);
+    }
+
+    function withdrawTokens(uint256 _id) external {
+        require(block.timestamp >= lockedToken[_id].unlockTime, 'Tokens are locked');
+        require(!lockedToken[_id].withdrawn, 'Tokens already withdrawn');
+        require(msg.sender == lockedToken[_id].withdrawalAddress, 'Can withdraw from the address used for locking');
+
+        address tokenAddress = lockedToken[_id].tokenAddress;
+        address withdrawalAddress = lockedToken[_id].withdrawalAddress;
+        uint256 amount = lockedToken[_id].tokenAmount;
+
+        require(IBEP20(tokenAddress).transfer(withdrawalAddress, amount), 'Failed to transfer tokens');
+
+        lockedToken[_id].withdrawn = true;
+        uint256 previousBalance = walletTokenBalance[tokenAddress][msg.sender];
+        walletTokenBalance[tokenAddress][msg.sender] = previousBalance.sub(amount);
+
+        // Remove depositId from withdrawal addresses mapping
+        uint256 i;
+        uint256 j;
+        uint256 byWLength = depositsByWithdrawalAddress[withdrawalAddress].length;
+        uint256[] memory newDepositsByWithdrawal = new uint256[](byWLength - 1);
+
+        for (j = 0; j < byWLength; j++) {
+            if (depositsByWithdrawalAddress[withdrawalAddress][j] == _id) {
+                for (i = j; i < byWLength - 1; i++) {
+                    newDepositsByWithdrawal[i] = depositsByWithdrawalAddress[withdrawalAddress][i + 1];
+                }
+                break;
+            } else {
+                newDepositsByWithdrawal[j] = depositsByWithdrawalAddress[withdrawalAddress][j];
+            }
+        }
+        depositsByWithdrawalAddress[withdrawalAddress] = newDepositsByWithdrawal;
+
+        // Remove depositId from tokens mapping
+        uint256 byTLength = depositsByTokenAddress[tokenAddress].length;
+        uint256[] memory newDepositsByToken = new uint256[](byTLength - 1);
+        for (j = 0; j < byTLength; j++) {
+            if (depositsByTokenAddress[tokenAddress][j] == _id) {
+                for (i = j; i < byTLength - 1; i++) {
+                    newDepositsByToken[i] = depositsByTokenAddress[tokenAddress][i + 1];
+                }
+                break;
+            } else {
+                newDepositsByToken[j] = depositsByTokenAddress[tokenAddress][j];
+            }
+        }
+        depositsByTokenAddress[tokenAddress] = newDepositsByToken;
+
+        emit TokensWithdrawn(tokenAddress, withdrawalAddress, amount);
+    }
+
+    function getTotalTokenBalance(address _tokenAddress) view public returns (uint256)
+    {
+        return IBEP20(_tokenAddress).balanceOf(address(this));
+    }
+
+    function getTokenBalanceByAddress(address _tokenAddress, address _walletAddress) view public returns (uint256)
+    {
+        return walletTokenBalance[_tokenAddress][_walletAddress];
+    }
+
+    function getAllDepositIds() view public returns (uint256[] memory)
+    {
+        return allDepositIds;
+    }
+
+    function getDepositDetails(uint256 _id) view public returns (address, address, uint256, uint256, bool)
+    {
+        return (lockedToken[_id].tokenAddress, lockedToken[_id].withdrawalAddress, lockedToken[_id].tokenAmount,
+        lockedToken[_id].unlockTime, lockedToken[_id].withdrawn);
+    }
+
+    function getDepositsByWithdrawalAddress(address _withdrawalAddress) view public returns (uint256[] memory)
+    {
+        return depositsByWithdrawalAddress[_withdrawalAddress];
+    }
+
+    function getDepositsByTokenAddress(address _tokenAddress) view public returns (uint256[] memory)
+    {
+        return depositsByTokenAddress[_tokenAddress];
+    }
+
+    function setBnbFee(uint256 fee) external onlyOwner {
+        require(fee > 0, 'Fee is too small');
+        bnbFee = fee;
+    }
+
+    function setLpFee(uint256 percent) external onlyOwner {
+        require(percent > 0, 'Percent is too small');
+        lpFeePercent = percent;
+    }
+
+    function withdrawFees(address payable withdrawalAddress) external onlyOwner {
+        if (remainingBnbFees > 0) {
+            withdrawalAddress.transfer(remainingBnbFees);
+            remainingBnbFees = 0;
+        }
+
+        for (uint i = 1; i <= tokenAddressesWithFees.length; i++) {
+            address tokenAddress = tokenAddressesWithFees[tokenAddressesWithFees.length - i];
+            uint256 amount = tokensFees[tokenAddress];
+            if (amount > 0) {
+                IBEP20(tokenAddress).transfer(withdrawalAddress, amount);
+            }
+            delete tokensFees[tokenAddress];
+            tokenAddressesWithFees.pop();
+        }
+
+        tokenAddressesWithFees = new address[](0);
+    }
+}
