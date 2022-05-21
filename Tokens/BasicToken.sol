ec59cf24dfe012f8c3a69d5139a19a520b16b6b1
Nathan A <85774641+Crypto-Nax@users.noreply.github.com>
2022-05-20 22:22:34 -0700

    Create BasicToken.sol

---
+++ Tokens/BasicToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+
+    constructor(uint startingSupply) ERC20("tokenName", "tokenSymbol") {
+        _mint(_msgSender(), startingSupply);
+    }
+}
